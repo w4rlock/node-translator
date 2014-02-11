695595d1a5152d4574ef9974f0624a5fcb5ab239
w4rlock <warlock.gpl@gmail.com>
2014-02-11 14:33:41 -0300

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     Ticketek = require('./concert.js').Ticketek,
     app = express(),
     Cache = require('cache-storage'),
-    FileStorage = require('cache-storage/Storage/FileSyncStorage'),
+    FileStorage = require('cache-storage/Storage/FileSyncStorage')
 
 app.use(express.logger());
 app.use(express.compress());
