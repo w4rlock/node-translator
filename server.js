34b87f07c90e8390e2b429372dfe5f027edf9b1b
w4rlock <warlock.gpl@gmail.com>
2014-02-11 14:25:30 -0300

    first commit

--- server.js
+++ server.js
@@ -1,63 +1,12 @@
-var express = require('express'),
-    request = require('request'),
-    cheerio = require('cheerio'),
-    Ticketek = require('./concert.js').Ticketek,
-    app = express(),
-    Cache = require('cache-storage'),
-    FileStorage = require('cache-storage/Storage/FileSyncStorage'),
-    server = require('http').createServer(app);
-
+var express = require('express');
+var app = express();
 app.use(express.logger());
-app.use(express.compress());
-app.use(express.methodOverride());
-app.use(express.bodyParser());
 
-var cache = new Cache(new FileStorage('./cache'), 'namespace');
-
-function cacheSave(key,value,expire){
-  var oexpires = { minutes: 10};
-  //moment({years: 2010, months: 3, days: 5, hours: 15, minutes: 10, seconds: 3, milliseconds: 123});
-  cache.save(key.toUpperCase(),value, {expire: oexpires});
-}
-
-function cacheGet(key){
-  return cache.load(key.toUpperCase());
-}
-
-
-//------------------------- ROUTES ------------------------------//
-app.use(express.static(__dirname + '/public'));
-
-app.get('/concerto/:evento', function(req, res){
-  var cach = cacheGet(req.params["evento"]);
-  if (cach == null){
-    var con = new Ticketek(req.params);
-    con.getAllPage(function(result){ 
-      cacheSave(req.params["evento"], result, null);
-      res.json(result);
-    });
-  }
-  else{
-    res.send(cach);
-  }
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.get('/concerto/:evento/:numpage', function(req, res){
-  var con = new Ticketek(req.params);
-  con.getPage(
-    function(result){ 
-      res.json(result);
-    },2);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-app.get('/', function(req, res) {
-  res.send(app.routes);
-});
-
-app.get('/*', function(req, res){
-  res.sendfile('./public/404.html');
-});
-//------------------------- ROUTES ------------------------------//
-
-server.listen(8081, '127.0.0.1', null);
-console.log('Server listen on 8080');
