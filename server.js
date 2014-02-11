67937863acb3e280dd110a629aa24c3e70e9a510
w4rlock <warlock.gpl@gmail.com>
2014-02-11 13:03:34 -0300

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
-var request = require("request");
-var cheerio = require("cheerio");
-var Ticketek = require('./concert.js').Ticketek;
-var Cache = require('cache-storage'),
-    FileStorage = require('cache-storage/Storage/FileSyncStorage');
+var express = require('express'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    param = require('express-params'),
+    Ticketek = require('./concert.js').Ticketek,
+    app = express(),
+    Cache = require('cache-storage'),
+    FileStorage = require('cache-storage/Storage/FileSyncStorage'),
+    server = require('http').createServer(app);
 
 var cache = new Cache(new FileStorage('./cache'), 'namespace');
 
@@ -21,179 +21,39 @@ function cacheGet(key){
 }
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+//------------------------- ROUTES ------------------------------//
+app.use(express.static(__dirname + '/public'));
 
-    //  Scope.
-    var self = this;
+app.get('/concerto/:evento', function(req, res){
+  var cach = cacheGet(req.params["evento"]);
+  if (cach == null){
+    var con = new Ticketek(req.params);
+    con.getAllPage(function(result){ 
+      cacheSave(req.params["evento"], result, null);
+      res.json(result);
+    });
+  }
+  else{
+    res.send(cach);
+  }
+});
 
+app.get('/concerto/:evento/:numpage', function(req, res){
+  var con = new Ticketek(req.params);
+  con.getPage(
+    function(result){ 
+      res.json(result);
+    },2);
+});
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+app.get('/', function(req, res) {
+  res.send(app.routes);
+});
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.get('/*', function(req, res){
+  res.sendfile('./public/404.html');
+});
+//------------------------- ROUTES ------------------------------//
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/concerto/:evento'] = function(req, res){
-          var cach = cacheGet(req.params["evento"]);
-          if (cach == null){
-            var con = new Ticketek(req.params);
-            con.getAllPage(function(result){ 
-              cacheSave(req.params["evento"], result, null);
-              res.json(result);
-            });
-          }
-          else{
-            res.send(cach);
-          }
-        };
-
-        self.routes['/concert'] = function(req, res) {
-          if (resp != undefined && resp.conciertos != undefined){
-            resp.cantidad = 0;
-            resp.conciertos = [];
-          }
-          miRequest = res;
-          request({ uri: url}, onCompleteLoad);
-        };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+server.listen(8081);
+console.log('Server listen on 8081');
